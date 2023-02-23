acf488dd8094320b387a737f8b8106091a5c462d
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-02-23 18:27:52 -0500

    updated switch case

--- js/index.js
+++ js/index.js
@@ -184,17 +184,17 @@ window.onload = () => {
 
   document.addEventListener('keydown', e => {
     switch (e.keyCode) {
-      case 38:
-        player.moveUp();
-        break;
-      case 40:
-        player.moveDown();
-        break;
       case 37:
         player.moveLeft();
         break;
+      case 38:
+        player.moveUp();
+        break;
       case 39:
         player.moveRight();
+        break;
+      case 40:
+        player.moveDown();
         break;
     }
 
